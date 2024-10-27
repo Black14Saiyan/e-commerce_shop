87e313320fd975fe2537f35cff3e6e38eb0db8b8
taif cisse <cissetaif3@gmail.com>
2024-10-27 10:30:28 +0000

    Firts commit of shop app

---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+
+
+# CREER UN MODELE D'UTILISATEUR POUR LA GESTION DES USERS (SHOPPER = ACHETEUR)
+class Shopper(AbstractUser):
+	pass
---
+++ store/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from store.models import Product
+
+# AJOUTER NORTE MODELE DE GESTION DES PRODUITS DANS L'INTERFACE ADMIN.
+admin.site.register(Product)
