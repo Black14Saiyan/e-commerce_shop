c0dc9a450a77a65d57a6a00508b844d7ee9edd7a
taif cisse <cissetaif3@gmail.com>
2024-10-28 13:25:53 +0000

    distribussion url avec affichage des images sur le index.html

--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render #type: ignore
+from store.models import Product
 
-# Create your views here.
+# CHEMIN POUR RETOURNER LA PAGE INDEX
+def index(request):
+    # POUR RECUPERER LES ELEMENTS DU MODELE PRODUCT
+    products = Product.objects.all()
+    return render(request, 'store/index.html', context={"products" : products})
